82574f16de50821741eb0687af0658c0936217b5
Андрей Гордиевский <agwzdushaz@gmail.com>
2025-05-26 22:05:07 +0700

    Реализована настройка ошибки для нулевого колличества продукта

--- src/class_module.py
+++ src/class_module.py
@@ -11,6 +11,8 @@ class Product(BaseProduct, MixinPrint):
         self.description = description
         self.__price = price
         self.quantity = quantity
+        if self.quantity == 0:
+            raise ValueError("Возникла ошибка ValueError прерывающая работу программы при попытке добавить продукт с нулевым количеством")
         super().__init__()
 
 
@@ -90,7 +92,6 @@ class Smartphone(Product):
         self.memory = memory
         self.color = color
 
-
     def __add__(self, other: "Smartphone | LawnGrass | Product") -> int:
         """Метод складывания количества товаров на складе"""
         if type(self) is not type(other):
@@ -121,7 +122,7 @@ class LawnGrass(Product):
         if type(self) is not type(other):
             raise TypeError("Возникла ошибка TypeError при добавлении не продукта")
         return self.quantity + other.quantity
-
+# print(LawnGrass.__mro__)
 
 class Category(FormationProduct):
     """Класс категории с описанием и счетчиком продуктов"""
@@ -192,6 +193,7 @@ class Category(FormationProduct):
         return len(self.__products)
 
 
+
 class Iterator:
     """Класс для итерации и показ товар категории"""
 
@@ -232,34 +234,34 @@ class Order(FormationProduct):
 # if __name__ == "__main__":
     # product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     # product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    # product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-    # grass1 = LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
-    # grass2 = LawnGrass("Газонная трава 2", "Выносливая трава", 450.0, 15, "США", "5 дней", "Темно-зеленый")
-    # smartphone1 = Smartphone(
-    #     "Samsung Galaxy S23 Ultra",
-    #     "256GB, Серый цвет, 200MP камера",
-    #     180000.0,
-    #     5, 95.5,
-    #     "S23 Ultra",
-    #     256,
-    #     "Серый"
-    # )
-    # smartphone2 = Smartphone("Iphone 15",
-    #                          "512GB, Gray space",
-    #                          210000.0,
-    #                          8,
-    #                          98.2,
-    #                          "15",
-    #                          512,
-    #                          "Gray space")
-    #
-    #
-    # prod_dict = {
-    #     "name": "Samsung Galaxy C23 Ultra",
-    #     "description": "256GB, Серый цвет, 200MP камера",
-    #     "price": 210000.0,
-    #     "quantity": 5
-    # }
+#     # product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+#     # grass1 = LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
+#     # grass2 = LawnGrass("Газонная трава 2", "Выносливая трава", 450.0, 15, "США", "5 дней", "Темно-зеленый")
+#     smartphone1 = Smartphone(
+#         "Samsung Galaxy S23 Ultra",
+#         "256GB, Серый цвет, 200MP камера",
+#         180000.0,
+#         5, 95.5,
+#         "S23 Ultra",
+#         256,
+#         "Серый"
+#     )
+#     smartphone2 = Smartphone("Iphone 15",
+#                              "512GB, Gray space",
+#                              210000.0,
+#                              8,
+#                              98.2,
+#                              "15",
+#                              512,
+#                              "Gray space")
+#
+#
+#     prod_dict = {
+#         "name": "Samsung Galaxy C23 Ultra",
+#         "description": "256GB, Серый цвет, 200MP камера",
+#         "price": 210000.0,
+#         "quantity": 5
+#     }
     #
     # prod_list1 = [product1, product2]
     # prod_list2 = [smartphone1, smartphone2]
